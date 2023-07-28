a5322a543deb4411f15ee593582a2a02933a5ffc
Richard Bundock <rbundock@cohaesus.co.uk>
2023-07-28 16:18:11 +0200

    Fixed env issue. Changed to review contents ignore tags.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import openai
 import os
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
+load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')   # Set your OpenAI API key
 
 def extract_data_from_html(url, prompt):
@@ -12,11 +14,14 @@ def extract_data_from_html(url, prompt):
     # Parse the website content
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    output = soup.prettify()
+    output = soup.text
+
+    #print(output)
+    #exit
 
     # Pass the HTML content and the prompt to the language model
     response = openai.Completion.create(
-        model="gpt-3.5-turbo-16k",  # Use the most advanced model available
+        model="text-davinci-003",  # Use the most advanced model available
         prompt=f"{output}\n{prompt}",
         temperature=0.5,  # Adjust as needed for more or less randomness
         max_tokens=200  # Adjust as needed based on how much data you expect to extract
@@ -26,5 +31,5 @@ def extract_data_from_html(url, prompt):
     print(response.choices[0].text.strip())
 
 url = "https://cohaesus.co.uk/blog/guide-to-ecommerce-replatforming/"  # Replace with your desired URL
-prompt = "Extract the main text from this webpage."  # Replace with your desired instruction
+prompt = "Structure the content of this file into a json schema"  # Replace with your desired instruction
 extract_data_from_html(url, prompt)
